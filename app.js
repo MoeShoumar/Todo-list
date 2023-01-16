f35a437991fe0db7d3d6ec2b65647c53c7d52c13
Mohamad Shoumar <mks38@mail.aub.edu>
2023-01-16 17:58:06 +0200

    added console.log for more accessibility

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 let input = prompt("What would you do?")
 let myArray = []
-if (input === "new") {
-    addTodo = prompt("Enter new Todo");
-    myArray.push(addTodo);
-}
-else if (input === "list") {
-    for (let element of myArray) {
-        console.log(element);
+while (input !== 'quit') {
+    if (input === "new") {
+        addTodo = prompt("Enter new Todo");
+        myArray.push(addTodo);
+        console.log()
     }
-else if (input === "delete") {
-        deleteIndex = prompt("enter the index of todo to delete")
-        for (let index of )
-            myArray.pop(deleteIndex);
+    else if (input === "list") {
+        for (let element of myArray)
+            console.log(`element: ${myArray[element]}`);
     }
-    else if (input === "quit") {
-        break;
-    }
+    //else if (input === "delete") {
+    //deleteIndex = prompt("enter the index of todo to delete")
+    //for (let index of )
+    //myArray.pop(deleteIndex);
+    //}
+    else if (input === "quit") break;
     else {
-        "Please, enter a valid command"
+        "Please, enter a valid command";
     }
+}
 
+if (input === quit) {
+    console.log("You quit")
+}
+else {
 
-
+}
