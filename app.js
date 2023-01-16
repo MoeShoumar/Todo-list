a7b070a4e670e35acc6756f0d5f6ce2a1f92737a
Mohamad Shoumar <mks38@mail.aub.edu>
2023-01-16 20:01:55 +0200

    fixed the infinite while loop buy adding an input at last

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
 let input = prompt("What would you do?")
 let myArray = []
 while (input !== 'quit') {
-    if (input === "new") {
-        addTodo = prompt("Enter new Todo");
-        myArray.push(addTodo);
-        console.log(`${addTodo} added to the list `)
-    }
-    else if (input === "list") {
+    if (input === "list") {
         for (let [index, element] of myArray.entries()) {
             console.log(`${index}, ${element}`);
         }
+    }
+
+    else if (input === "new") {
+        addTodo = prompt("Enter new Todo");
+        myArray.push(addTodo);
+        console.log(`${addTodo} added to the list `)
     }
     else if (input === "delete") {
         deleteIndex = prompt("enter the index of todo to delete")
         myArray.splice(deleteIndex, 1);
     }
-    else if (input === "quit") break;
-
-    else {
-        "Please, enter a valid command";
-    }
+    input = prompt('what would you like to do?')
 }
-input = prompt("What would you do?")
-
 
 console.log('OK quit the app')
